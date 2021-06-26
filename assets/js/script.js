5ce5c5a1cd6e2ec48b24e15f2b7156505a9be30f
Mason021 <mdc5054@gmail.com>
2021-06-26 16:11:15 -0500

    I have the info entered showing up in the console and i've set that to a var of its own but need to figure out how to keep it there after refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,9 +52,39 @@ timeColors();
 // THEN the text for that event is saved in local storage
 
 function saveEnteredText() {
-    // var enteredInfo = 
-    console.log("hello")
-    console.log(this.previousElementSibling.value)
-}
 
+    var enteredText = (this.previousElementSibling.value);
+
+    console.log(enteredText);
+
+    localStorage.setItem("what was said", enteredText);
+};
+
+
+    // localStorage.getItem("what was said");
+
+    // if (enteredText !== null) {
+    //     var textSticky = JSON.parse(enteredText);
+    // }
+    
+    
+    // localStorage.setItem(actualCurrentTime, enteredInfo)
+    // localStorage.setItem(enteredInfo, )
+    // console.log("hello")
+    // console.log(this.previousElementSibling.value)
+    // console.log(enteredInfo)
+
+
+// Get item from local storage if any
+// $("#9 .description").val(localStorage.getItem("9"));
+// $("#10 .description").val(localStorage.getItem("10"));
+// $("#11 .description").val(localStorage.getItem("11"));
+// $("#12 .description").val(localStorage.getItem("12"));
+// $("#13 .description").val(localStorage.getItem("13"));
+// $("#14 .description").val(localStorage.getItem("14"));
+// $("#15 .description").val(localStorage.getItem("15"));
+// $("#16 .description").val(localStorage.getItem("16"));
+// $("#17 .description").val(localStorage.getItem("17"));
+
+// click event //
 saveBtn.on("click", saveEnteredText)
